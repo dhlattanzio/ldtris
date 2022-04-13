3e32a88073ac3143b07e2b7772e319a0b142f54c
Diego Lattanzio <lattanzio.diego@gmail.com>
2022-04-13 02:02:18 -0300

    los bloques ahora se apilan

--- game.js
+++ game.js
@@ -13,47 +13,113 @@ const ctx = canvas.getContext("2d");
 
 const image = document.getElementById('source');
 
-let board = Array(BOARD_HEIGHT).fill().map(() => Array(BOARD_WIDTH).fill(0));
-
 class Game {
     constructor(ctx) {
         this.ctx = ctx;
         this.board = new Board(10, 20);
+        this.downTime = 0.3;
 
-        this.block = new Block([[0, 1], [1, 1], [2, 1], [2, 0]])
+        this.nextBlock();
+    }
+
+    moveBlock(move) {
+        this.block.x = Math.min(Math.max(this.block.x + move, 0), this.board.cols - this.block.width);
+    }
+
+    rotateBlock(dir) {
+    }
+
+    downBlock() {
+        const prevY = this.block.y;
+        const nextY = Math.max(this.block.height - 1, this.block.y - 1);
+
+        this.block.y = nextY;
+        if (this.board.overlap(this.block)) {
+            this.block.y = prevY;
+        }
+        
+        return prevY != this.block.y;
+    }
+
+    fastDownBlock() {
+
+    }
+
+    instantDownBlock() {
+
+    }
+
+    createBlock() {
+        const blocks = {
+            0: new Block([[0, 0], [1, 0], [0, 1], [1, 1]]), // O
+            1: new Block([[0, 0], [0, 1], [0, 2], [0, 3]]), // I
+            2: new Block([[1, 0], [2, 0], [0, 1], [1, 1]]), // S
+            3: new Block([[0, 0], [1, 0], [1, 1], [2, 1]]), // Z
+            4: new Block([[0, 0], [0, 1], [0, 2], [1, 2]]), // L
+            5: new Block([[1, 0], [1, 1], [0, 2], [1, 2]]), // J
+            6: new Block([[0, 1], [1, 1], [2, 1], [1, 0]])  // T
+        }
+        const randomNumber = Math.floor(Math.random() * Object.keys(blocks).length);
+
+        return blocks[randomNumber];
+    }
+
+    nextBlock() {
+        const prevX = this.block != null ? this.block.x : 0;
+        this.block = this.createBlock();
+        this.block.x = prevX;
         this.block.y = this.board.rows - 1;
     }
 
     draw() {
-        // this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const deltaTime = 1.0 / 60.0;
+
         this.ctx.fillStyle = "lightgray";
         this.ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+        // all board
+        const size = (BLOCK_SIZE + PADDING);
         for(let y=0;y<this.board.rows;y++) {
+            let blockY = (this.board.rows - y - 1) * size;
             for(let x=0;x<this.board.cols;x++) {
                 this.ctx.strokeStyle = "white";
                 this.ctx.lineWidth = 1;
                 this.ctx.strokeRect(
-                    x * BLOCK_SIZE + (x * PADDING),
-                    y * BLOCK_SIZE + (y * PADDING),
-                    BLOCK_SIZE + PADDING, BLOCK_SIZE + PADDING
+                    x * size,
+                    blockY,
+                    size, size
                 );
+
+                if (this.board.tiles[y][x] > 0) {
+                    this.ctx.drawImage(image,
+                        x * size, blockY,
+                        size, size);
+                }
             }
         }
 
-        // void ctx.drawImage(image, dx, dy, dWidth, dHeight);
-        const mult = BLOCK_SIZE + PADDING;
+        // current block
         for(const tile of this.block.tiles) {
             this.ctx.drawImage(image,
-                (this.block.x + tile[0]) * mult,
-                ((this.board.rows - 1 + tile[1]) * mult) - (this.block.y * mult),
+                (this.block.x + tile[0]) * size,
+                ((this.board.rows - 1 + tile[1]) * size) - (this.block.y * size),
                 BLOCK_SIZE, BLOCK_SIZE);
         }
 
-        this.block.y = Math.max(0, this.block.y - 1);
+        // tmp: down block every x time
+        this.downTime -= deltaTime;
+        if (this.downTime <= 0) {
+            const stepDown = this.downBlock();
+            if (!stepDown) {
+                this.board.putBlock(this.block);
+                this.nextBlock();
+                // TODO
+            }
 
-        console.log("das");
-        setTimeout(() => requestAnimationFrame(this.draw.bind(this)), 17 * 40);
+            this.downTime = 0.6;
+        }
+
+        setTimeout(() => requestAnimationFrame(this.draw.bind(this)), 17);
     }
 }
 
@@ -61,6 +127,21 @@ class Board {
     constructor(cols, rows) {
         this.cols = cols;
         this.rows = rows;
+        this.tiles = Array(rows).fill().map(() => Array(cols).fill(0));
+    }
+
+    overlap(block) {
+        for(const tile of block.tiles) {
+            if (this.tiles[block.y - tile[1]][tile[0] + block.x] > 0) return true;
+        }
+        return false;
+    }
+
+    putBlock(block) {
+        console.log("put", block);
+        for(const tile of block.tiles) {
+            this.tiles[block.y - tile[1]][tile[0] + block.x] = 1;
+        }
     }
 }
 
@@ -76,8 +157,8 @@ class Block {
 
     updateSize() {
         const size = this.tiles.reduce((acc, tile) => {
-            acc[0] = Math.max(acc[0], tile[0]);
-            acc[1] = Math.max(acc[1], tile[1]);
+            acc[0] = Math.max(acc[0], tile[0] + 1);
+            acc[1] = Math.max(acc[1], tile[1] + 1);
             return acc;
         }, [0, 0]);
 
@@ -104,3 +185,24 @@ class Block {
 
 const game = new Game(ctx);
 game.draw();
+
+document.addEventListener('keydown', event => {
+    switch(event.key) {
+        case "ArrowRight":
+            game.moveBlock(1);
+            break
+        case "ArrowLeft":
+            game.moveBlock(-1);
+            break
+        case "ArrowDown":
+            game.fastDownBlock();
+            break
+        case "ArrowUp":
+            game.instantDownBlock();
+            break
+    }
+})
+
+document.addEventListener("keypress", event => {
+
+})
